91765a0466faf6bb286daf25771a3cebc216166c
Jack <admin@mbp2013.local>
2016-03-21 11:21:52 +0800

    Initial Commit

---
+++ Tracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tracker
+//
+//  Created by admin on 21/3/2016.
+//  Copyright © 2016 idt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tracker
+//
+//  Created by admin on 21/3/2016.
+//  Copyright © 2016 idt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
